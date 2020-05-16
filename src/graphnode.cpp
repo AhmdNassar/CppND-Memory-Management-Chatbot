3bc3b47ae554f86b75ffd73e91385ac0b8acff49
AhmdNassar <engahmdnassar@gmail.com>
2020-05-16 07:19:04 +0200

    fix core dump error(warm up task)

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,7 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    //delete _chatBot; node don't own chatBot to delete it.
 
     ////
     //// EOF STUDENT CODE
